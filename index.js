58b25ef83c6305085296946d2a79ecb85b328905
Amarildo Vieira <amarildoricardovieira@gmail.com>
2024-04-21 02:43:56 +0100

    Coloquei os links finais e iniciei as configurações básicas do javaScript

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("Amarildo Vieira");
+let inicio = window.document.getElementsByClassName('ini')
+let contat = window.document.getElementsByClassName('cont')
+let sobre = window.document.getElementsByClassName('sob')
+let servico = window.document.getElementsByClassName('meu')
