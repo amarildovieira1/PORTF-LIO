84be22729266e58dde421c60cfea7637fc92a3a0
Amarildo Vieira <amarildoricardovieira@gmail.com>
2024-04-21 03:39:43 +0100

    Terminei os acabamentos e confiigurações finais do portfólio. Restando apenas a alteração dos lorem

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-console.log("Amarildo Vieira");
-let inicio = window.document.getElementsByClassName('ini')
-let contat = window.document.getElementsByClassName('cont')
-let sobre = window.document.getElementsByClassName('sob')
-let servico = window.document.getElementsByClassName('meu')
+
+
+function ini() {
+    window.scrollBy(0, 0)   
+}
+
+function sob() {
+    window.scrollBy(0, 880) 
+}
+
+function meu() {
+    window.scrollBy(0, 1760)    
+}
+
+function cont() {
+    window.scrollBy(0, 1760)    
+}
+
+function inis() {
+    window.scrollTo(0, 0)   
+}
+
+function sobs() {
+    window.scrollTo(0, 880) 
+}
+
+function meus() {
+    window.scrollTo(0, 1320)    
+}
